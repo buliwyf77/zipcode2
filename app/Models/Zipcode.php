174f099b688986535778fc23ded1991a6f126deb
Luis Torrealba <luis@luistorrealba.com>
2022-08-08 10:46:41 -0400

    fix model to show visible

--- app/Models/Zipcode.php
+++ app/Models/Zipcode.php
@@ -21,4 +21,6 @@ class Zipcode extends Model
      * @var array
      */
     protected $fillable = ['d_codigo', 'd_asenta', 'd_tipo_asenta', 'D_mnpio', 'd_estado', 'd_ciudad', 'd_CP', 'c_estado', 'c_oficina', 'c_CP', 'c_tipo_asenta', 'c_mnpio', 'id_asenta_cpcons', 'd_zona', 'c_cve_ciudad'];
+
+    protected $visible = ['d_codigo', 'd_asenta', 'd_tipo_asenta', 'D_mnpio', 'd_estado', 'd_ciudad', 'd_CP', 'c_estado', 'c_oficina', 'c_CP', 'c_tipo_asenta', 'c_mnpio', 'id_asenta_cpcons', 'd_zona', 'c_cve_ciudad'];
 }
